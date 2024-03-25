0d2347ec24df054923cc5385000d9a0aab03f357
Tay Jun Jie <jjat1987@gmail.com>
2024-03-26 00:21:48 +0800

    Debug NodeJS installation

--- setup_my_ubuntu.sh
+++ setup_my_ubuntu.sh
@@ -71,6 +71,9 @@ setup_my_ubuntu () {
 
   # Install NodeJS
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   nvm install 20
 
   # Install go
