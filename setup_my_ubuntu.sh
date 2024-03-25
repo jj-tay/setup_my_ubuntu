8a1d227ac32101c5be9aa5c191ee42f2f998ff43
Tay Jun Jie <jjat1987@gmail.com>
2024-03-25 23:55:06 +0800

    Make unzip quiet

--- setup_my_ubuntu.sh
+++ setup_my_ubuntu.sh
@@ -92,7 +92,7 @@ setup_my_ubuntu () {
 	# Install awscli
 	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 	sudo apt-get install -y unzip
-	unzip awscliv2.zip
+	unzip -q awscliv2.zip
 	sudo ./aws/install
 
   # Install tldr
