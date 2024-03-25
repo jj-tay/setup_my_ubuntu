716da3250bf94754e2eb38e263eb3b89e22d0567
Tay Jun Jie <jjat1987@gmail.com>
2024-03-25 23:34:29 +0800

    Debug neovim installation

--- setup_my_ubuntu.sh
+++ setup_my_ubuntu.sh
@@ -81,7 +81,7 @@ setup_my_ubuntu () {
 
 	# Install neovim
 	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-	sudo rm -rf /opt/nvim
+	sudo rm -rf /opt/nvim*
 	sudo tar -C /opt -xzf nvim-linux64.tar.gz
 	rm nvim-linux64.tar.gz
 
