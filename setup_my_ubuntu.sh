6aa3d5ea57d28100dd972a81d784d27e2a3305e5
Tay Jun Jie <jjat1987@gmail.com>
2024-03-25 23:22:39 +0800

    Debug Hashicorp keyring

--- setup_my_ubuntu.sh
+++ setup_my_ubuntu.sh
@@ -73,7 +73,7 @@ setup_my_ubuntu () {
   nvm install 20
 
 	# Install terraform and packer
-	wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 	echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 	sudo apt-get -y update
 	sudo apt-get install -y terraform packer
